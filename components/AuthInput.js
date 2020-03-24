3e4e0ab93d2f7092bde8d5306b249a4dd036f8a6
baejinnwoo <bjwkor@naver.com>
2020-03-24 17:38:35 +0900

    로그인

--- components/AuthInput.js
+++ components/AuthInput.js
@@ -1,16 +1,38 @@
 import React from "react";
 import styled from "styled-components";
+import constants from "../constants";
 
 const Container = styled.View`
-    margin-bottom:10px;
+  margin-bottom: 10px;
 `;
 
-const TextInput = styled.TextInput``;
+const TextInput = styled.TextInput`
+  width: ${constants.width / 1.6};
+  padding: 10px;
+  background-color: ${props => props.theme.lightGreyColor};
+  border: 1px solid ${props => props.theme.darkGreyColor};
+  border-radius: 4px;
+`;
 
-const AuthInput = ({placeholder,value,keyboardType="default"}) => (
-    <Container>
-        <TextInput placeholder={placeholder} value={value} keyboardType={keyboardType}/>
-    </Container>
-)
+const AuthInput = ({
+  autoCapitalize = "none",
+  placeholder,
+  value,
+  keyboardType = "default",
+  onChange,
+  onEndEditing = () => null,
+  autoCorrect = true
+}) => (
+  <Container>
+    <TextInput
+      autoCapitalize={autoCapitalize}
+      placeholder={placeholder}
+      keyboardType={keyboardType}
+      onChangeText={onChange}
+      onEndEditing={onEndEditing}
+      autoCorrect={autoCorrect}
+    />
+  </Container>
+);
 
 export default AuthInput;
--- screens/Auth/Login.js
+++ screens/Auth/Login.js
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import AuthButton from "../../components/AuthButton";
 import AuthInput from "../../components/AuthInput";
+import useInput from "../../hooks/useInput";
+import { Alert, TouchableWithoutFeedback ,Keyboard } from "react-native";
 
 const View = styled.View`
   justify-content: center;
@@ -11,9 +13,33 @@ const View = styled.View`
 
 const Text = styled.Text``;
 
-export default () => (
-  <View>
-    <AuthInput placeholder={"이메일 주소를 입력해 주세요."} value="" keyboardType="email-address"/>
-      <AuthButton text={"로그인"} onPress={()=> null}/>
-  </View>
-);
+export default () => {
+
+  const emailInput = useInput("");
+  
+  const handleLogin = () => {
+    const { value } = emailInput;
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (value === "") {
+      return Alert.alert("Email can't be empty");
+    } else if (!value.includes("@") || !value.includes(".")) {
+      return Alert.alert("Please write an email");
+    } else if (!emailRegex.test(value)) {
+      return Alert.alert("That email is invalid");
+    }
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+    <View>
+      <AuthInput
+        {...emailInput}
+        placeholder={"이메일 주소를 입력해 주세요."}
+        keyboardType="email-address"
+        onEndEditing={handleLogin}
+        autoCorrect={false}
+      />
+      <AuthButton text={"로그인"} onPress={handleLogin} />
+    </View>
+    </TouchableWithoutFeedback>
+  );};
