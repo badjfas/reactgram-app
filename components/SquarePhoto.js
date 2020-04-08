e7720ea5e08e6f585bafcc999f1d04cd92611061
배진우 <bjwkor@naver.com>
2020-04-08 19:08:19 +0900

    .

--- components/SquarePhoto.js
+++ components/SquarePhoto.js
@@ -4,14 +4,14 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 import { Image } from "react-native";
 import constants from "../constants";
 import { useNavigation } from "@react-navigation/native";
-const SquarePhoto = ({files=[],data}) => {
+const SquarePhoto = ({files=[],id}) => {
     const navigation = useNavigation();
-
+    console.log(id,"SqurePhoto");
     return(
-    <TouchableOpacity onPress={()=> {navigation.navigate("Detail",{data})}}>
+    <TouchableOpacity onPress={()=> {navigation.navigate("Detail",{id})}}>
       <Image
         source={{ uri: files[0].Url }}
-        style={{ width: constants.width / 3, height: constants.height / 4 }}
+        style={{ width: constants.width / 3, height: constants.height / 6 }}
       />
     </TouchableOpacity>
     )}
